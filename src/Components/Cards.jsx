dc464c2f1e96dc4839b3c304b0b1e124800e35fc
Erika Ladner <erikaladner5@gmail.com>
2023-03-31 16:56:45 -0300

    facebook

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -4,51 +4,48 @@ import img2 from './../imagenes/img2.jpg'
 import img3 from './../imagenes/img3.jpg'
 import './Cards.css'
 import { BiCommentDetail } from 'react-icons/bi';
+import { Button, Card } from 'antd';
+const { Meta } = Card;
 
 export default function Cards(){
     return(
-        <div className="artis">
-                  
-<body>
-	<div class="wrap">
-		<div class="tarjeta-wrap">
-			<div class="tarjeta">
-				<div class="adelante card1">
-                <img src={img1} className="img-fluid rounded-start" alt="..."/>
-                </div>
-				<div class="atras">
-                <h5>Plan de Escucha Vecinal</h5>
-					<p>👉Qué propones para mejorar la salud municipal?</p>
-                    <a href='https://www.facebook.com/photo?fbid=117669421275634&set=a.116619721380604'><BiCommentDetail className="comentar"/></a>
-				</div>
-			</div>
-		</div>
-		<div class="tarjeta-wrap">
-			<div class="tarjeta">
-				<div class="adelante card2">
-                <img src={img2} className="img-fluid rounded-start" alt="..."/>
-                </div>
-				<div class="atras">
-                <h5 >Plan de Escucha Vecinal</h5>
-					<p>👉Qué ideas tenés para controlar el problema de los microbasurales?</p>
-                    <a href='https://www.facebook.com/photo?fbid=121295144246395&set=a.116619721380604'><BiCommentDetail className="comentar"/></a>
-				</div>
-			</div>
-		</div>
-		<div class="tarjeta-wrap">
-			<div class="tarjeta">
-				<div class="adelante card3">
-                <img src={img3} className="img-fluid rounded-start" alt="..."/>
-                </div>
-				<div class="atras">
-                <h5>Plan de Escucha Vecinal</h5>
-					<p>👉¿Vos qué problemáticas priorizás en materia de seguridad?</p>
-                    <a href='https://www.facebook.com/photo?fbid=116619698047273&set=a.116619721380604'><BiCommentDetail className="comentar"/></a>
-				</div>
-			</div>
-		</div>
-	</div>
-</body>
+<div className="artis" id="cartas">
+
+  <Card
+    hoverable
+    style={{
+      width: 240,
+    }}
+    cover={<img alt="example" src={img1} className="img-carta"/>}
+	className="carta"
+  >
+    <Meta title="Plan de Escucha Vecinal" description="👉Qué propones para mejorar la salud municipal?" /><Button className="boton" href='https://www.facebook.com/photo/?fbid=125603043815605&set=a.116619721380604'>Opinar</Button>
+  </Card>
+
+
+  <Card
+    hoverable
+    style={{
+      width: 240,
+    }}
+    cover={<img alt="example" src={img2} className="img-carta"/>}
+	className="carta"
+  >
+    <Meta title="Plan de Escucha Vecinal" description="👉Qué ideas tenés para controlar el problema de los microbasurales?" /><Button className="boton" href='https://www.facebook.com/photo?fbid=121295144246395&set=a.116619721380604'>Opinar</Button>
+  </Card>
+
+
+  <Card
+    hoverable
+    style={{
+      width: 240,
+    }}
+    cover={<img alt="example" src={img3}  className="img-carta"/>}
+	className="carta"
+  >
+    <Meta title="Plan de Escucha Vecinal" description="👉¿Vos qué problemáticas priorizás en materia de seguridad?" /><Button className="boton" href='https://www.facebook.com/photo?fbid=116619698047273&set=a.116619721380604'>Opinar</Button>
+  </Card>
+
 
 </div>   
         
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,13 +14,25 @@ export default function Home(){
       {/*---Nav----*/}
     <nav class="navbar navbar-expand-lg bg-body-tertiary nav">
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <a class="navbar-brand" href="#">
           <img src={logo} alt='Accion Mar Platense' className="imglogo"/>
         </a>
-        
+        <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#cartas">Publicaciones</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#footer">Facebook</a>
+        </li>
+         </ul>
+         </div>
       </div>
     </nav>  
 
@@ -32,7 +44,7 @@ export default function Home(){
       
 
       {/*---Footer---*/}
-      <div className="footer">
+      <div className="footer" id='footer'>
         <a href="https://www.facebook.com/people/Plan-de-Escucha-Vecinal/100090977302524/"><BsFacebook className="facebook"/></a>
         <p>La Rioja 1545 | Mar del Plata | Buenos Aires | Argentina | Telefonos: 491-3515 / 494 4452</p>
         <p>© Copyright - All rights reserved.</p>
